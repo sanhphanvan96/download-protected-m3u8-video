7aefa197876a84be1b0b27d12af6f8f61bb08a5d
Sanh Phan Van <sanhphanvan96@gmail.com>
2020-04-18 23:41:35 +0700

    refactor

--- download.js
+++ download.js
@@ -8,37 +8,19 @@ var base_url = 'https://cdn.webtiengnhat.com'
 
 // get from browser's network tool
 // NOTES: change it
-var nUrl = base_url + '/list/pvdupload/smil:1544355855-3faa7fca8747da87d8cbfcd77647b130e856a2bf-5c0d000f0d0fb-ng-phap-n3-online-uchini-1.smil/1544355855-3faa7fca8747da87d8cbfcd77647b130e856a2bf-5c0d000f0d0fb-ng-phap-n3-online-uchini-1.mp4_chunk.m3u8?nimblesessionid=97020';
+var playlistUrl = base_url + '/list/pvdupload/smil:1544355855-3faa7fca8747da87d8cbfcd77647b130e856a2bf-5c0d000f0d0fb-ng-phap-n3-online-uchini-1.smil/1544355855-3faa7fca8747da87d8cbfcd77647b130e856a2bf-5c0d000f0d0fb-ng-phap-n3-online-uchini-1.mp4_chunk.m3u8?nimblesessionid=97160';
 
-async function downloadFile(url, path) {
-    console.log('Downloading ' + url);
-    const writer = fs.createWriteStream(path)
+var numOfParts = 0;
 
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
-    })
-
-    response.data.pipe(writer)
-
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve)
-        writer.on('error', reject)
-    })
-}
-
-var nNumOfParts = 0;
-
-function getPartUrl(num) {
-    return nUrl.replace('mp4_chunk.m3u8', 'mp4-n_' + num + '_0_0.ts');
+function getTsPart(num) {
+    return playlistUrl.replace('mp4_chunk.m3u8', 'mp4-n_' + num + '_0_0.ts');
 }
 
 async function getTsUrl(num) {
     let rs = await axios.get(base_url + "/video/touch.php");
     let token = rs.data;
     console.log({ token });
-    let tsUrl = getPartUrl(num) + "&token=" +
+    let tsUrl = getTsPart(num) + "&token=" +
         function (_token) {
             {
                 var c = "";
@@ -57,16 +39,12 @@ async function getTsUrl(num) {
     return tsUrl;
 };
 
-async function downloadTsFile(tsUrl) {
-    console.log({
-        'Downloading ts' : tsUrl
-    });
-    let filename = tsUrl.split('?')[0].split(':').reverse()[0].split('/')[1];
-    const filePath = path.resolve(__dirname, 'data', filename)
-    const writer = fs.createWriteStream(filePath)
+async function downloadFile(url, path) {
+    console.log('Downloading ' + url);
+    const writer = fs.createWriteStream(path)
 
     const response = await axios({
-        url: tsUrl,
+        url,
         method: 'GET',
         responseType: 'stream'
     })
@@ -92,9 +70,10 @@ function convertTs2Mp4() {
 }
 
 async function download(numOfParts) {
+
     // download m3u8 file
     let _m3u8Path = path.resolve(__dirname, 'data', '_in.m3u8')
-    await downloadFile(nUrl, _m3u8Path)
+    await downloadFile(playlistUrl, _m3u8Path)
 
     // regenerate m3u8 file
     let m3u8Path = path.resolve(__dirname, 'data', 'in.m3u8')
@@ -104,8 +83,8 @@ async function download(numOfParts) {
         }
         var result = data.replace(/\?nimblesessionid=[0-9]*/g, '')
         // get number of Ts files
-        nNumOfParts = data.match(/ts/gm).length
-        console.log({ nNumOfParts })
+        numOfParts = data.match(/ts/gm).length
+        console.log({ numOfParts })
 
         fs.writeFile(m3u8Path, result, 'utf8', function (err) {
             if (err) return console.log(err);
@@ -114,15 +93,23 @@ async function download(numOfParts) {
 
     // download hls key
     let hlsPath = path.resolve(__dirname, 'data', 'hls.key')
-    let hlsKeyurl = nUrl.replace(/\.smil\/.*m3u8/g, '.smil/hls.key')
+    let hlsKeyurl = playlistUrl.replace(/\.smil\/.*m3u8/g, '.smil/hls.key')
     console.log('hls key', hlsKeyurl)
     await downloadFile(hlsKeyurl, hlsPath)
 
     // download all Ts files
-    for (var i = 0; i < nNumOfParts; i++) {
+    for (var i = 0; i < numOfParts; i++) {
         try {
             let tsUrl = await getTsUrl(i)
-            await downloadTsFile(tsUrl);
+
+            // download a ts file
+            let filenameTs = tsUrl.split('?')[0].split(':').reverse()[0].split('/')[1];
+            const filePathTs = path.resolve(__dirname, 'data', filenameTs)
+            console.log({
+                'Downloading ts': tsUrl
+            });
+            await downloadFile(tsUrl, filePathTs)
+
         } catch (error) {
             console.log(error)
             process.exit()
@@ -134,4 +121,4 @@ async function download(numOfParts) {
     })
 }
 
-download(nNumOfParts);
+download(numOfParts);
